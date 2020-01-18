0b206c627f539b38194619c3a9546d4bfe7404a5
James Gartrell <jgartrel@gmail.com>
2020-01-18 12:34:47 -0800

    Centralize config into config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+config = {}
+config.station_cfg = {}
+config.station_cfg.ssid="ssid"
+config.station_cfg.pwd="password"
+config.station_cfg.save=true
+config.topic = "sensors"
+config.measurement = "environ"
+config.location = "PSB-2fl"
+config.hostname = wifi.sta.gethostname()
+
+
--- wifi.lua
+++ wifi.lua
@@ -1,8 +1,4 @@
 wifi.setmode(wifi.STATION)
-station_cfg = {}
-station_cfg.ssid="ssid"
-station_cfg.pwd="password"
-station_cfg.save=true
-wifi.sta.config(station_cfg)
+wifi.sta.config(config.station_cfg)
 wifi.sta.connect()
 print(wifi.sta.getip())
