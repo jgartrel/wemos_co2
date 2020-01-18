1a139b2463ba4e94202f1dfced3fddd3d978d9fd
James Gartrell <jgartrel@gmail.com>
2020-01-18 14:24:09 -0800

    Failed attempt to wait for IP

--- wifi.lua
+++ wifi.lua
@@ -1,4 +1,24 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config(config.station_cfg)
 wifi.sta.connect()
-print(wifi.sta.getip())
+tmr.delay(1000000)
+
+function wait_ip()
+    print("Waiting for IP")
+
+    local max=15
+    while(wifi.sta.getip() == nil)
+    do
+        max=max-1
+        tmr.delay(1000000)
+        print(string.format('WiFi attempt %d', max))
+        if max==0 then
+        return
+        end
+    end
+
+    print("Waited for IP")
+    print(wifi.sta.getip())
+end
+
+wait_ip()
