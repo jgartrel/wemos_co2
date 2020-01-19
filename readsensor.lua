dafca1d4e2bf85f3c89f1a5a2fab8ed188b4173b
James Gartrell <jgartrel@gmail.com>
2020-01-19 00:35:42 -0800

    Locally scope variables for readsensor

--- readsensor.lua
+++ readsensor.lua
@@ -1,25 +1,25 @@
-topic = config.readsensor.topic
-measurement = config.readsensor.measurement
-location = config.readsensor.location
-hostname = config.readsensor.hostname
-tag_set = string.format("location=%s,sensor=%s", location, hostname)
-parse_field = {
-  ["H"] = function (v) return "humidity", tonumber(v) / 100 end,
-  ["T"] = function (v) return "temperature", tonumber(v) / 100 end,
-  ["Z"] = function (v) return "co2", tonumber(v) end,
-}
 
 function readsensor_enable()
   uart.setup(0,9600,8,0,1,0)
+  local topic = config.readsensor.topic
+  local measurement = config.readsensor.measurement
+  local location = config.readsensor.location
+  local hostname = config.readsensor.hostname
+  local tag_set = string.format("location=%s,sensor=%s", location, hostname)
+  local parse_field = {
+    ["H"] = function (v) return "humidity", tonumber(v) / 100 end,
+    ["T"] = function (v) return "temperature", tonumber(v) / 100 end,
+    ["Z"] = function (v) return "co2", tonumber(v) end,
+  }
   uart.on("data","\r", function(data)
     if global_c~=nil then
-      fields = {}
+      local fields = {}
       string.gsub(data, "(%a)%s(%d+)", function (k,v)
         if parse_field[k] then
           table.insert(fields,string.format("%s=%s",parse_field[k](v)))
         end
       end)
-      field_set = table.concat(fields,",")
+      local field_set = table.concat(fields,",")
       if field_set ~= "" then
         global_wd_data = true
         global_c:publish(topic, string.format("%s,%s %s", measurement, tag_set, field_set), 0, 0)
